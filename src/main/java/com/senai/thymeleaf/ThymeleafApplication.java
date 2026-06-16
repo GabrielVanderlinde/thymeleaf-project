1576171e9d898a28ce2ee416b8d1f4f122c538a8
GABRIEL HENRIQUE VANDERLINDE <gabriel_vanderlinde@estudante.sesisenai.org.br>
2026-06-16 20:06:17 -0300

    chore: corrije arquivo application

--- src/main/java/com/senai/thymeleaf/ThymeleafApplication.java
+++ src/main/java/com/senai/thymeleaf/ThymeleafApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ThymeleafApplication {
 
-    static void main(String[] args) {
+ public static void main(String[] args) {
         SpringApplication.run(ThymeleafApplication.class, args);
     }
 
