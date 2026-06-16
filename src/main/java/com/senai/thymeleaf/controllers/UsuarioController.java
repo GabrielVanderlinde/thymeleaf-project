05c22fadca80f6b42e07230f6449e169c242f2bb
GABRIEL HENRIQUE VANDERLINDE <gabriel_vanderlinde@estudante.sesisenai.org.br>
2026-06-16 20:17:17 -0300

    fix: corrije problemas relacionados a exclusao de usuarios

--- src/main/java/com/senai/thymeleaf/controllers/UsuarioController.java
+++ src/main/java/com/senai/thymeleaf/controllers/UsuarioController.java
@@ -3,11 +3,15 @@ package com.senai.thymeleaf.controllers;
 import com.senai.thymeleaf.dtos.UsuarioDto;
 import com.senai.thymeleaf.services.UsuarioService;
 import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -53,5 +57,12 @@ public class UsuarioController {
         service.usuarioInserir(usuarioDto);
         redirectAttributes.addFlashAttribute("mensagem", "Usuário cadastrado com sucesso!");
         return "redirect:/usuariolista";
+    }
+
+    @DeleteMapping("/usuarioexcluir/{id}")
+    @ResponseBody
+    public ResponseEntity<Void> excluirUsuario(@PathVariable Long id) {
+        service.excluir(id);
+        return ResponseEntity.ok().build();
     }
 }
